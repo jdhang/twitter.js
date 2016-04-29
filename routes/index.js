48e54e85ed4244251dc25b42625e2c7d5a61e502
Jason Hang <dev.jdhang@gmail.com>
2016-04-29 18:04:36 -0400

    Added promisified client query function to '/ ' and '/users/:name'.

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express'),
-    router = express.Router(),
-    path = require('path'),
-    tweetBank = require('../tweetBank')
+  router = express.Router(),
+  path = require('path'),
+  Promise = require('bluebird')
 
-module.exports = function (io, client) {
+module.exports = function(io, client) {
 
-  router.get('/', function (req, res) {
-    client.query('SELECT tweets.id, tweets.content, users.name, users.pictureurl FROM Tweets JOIN users ON users.id = Tweets.userid', function (err, result) {
-      var tweets = result.rows;
+  function clientQuery (query, array) {
+    return new Promise (function (resolve, reject) {
+      client.query(query, array, function (err, result) {
+        if (err) reject(err)
+        else resolve(result)
+      })
+    }) 
+  }
+
+  router.get('/', function(req, res) {
+    clientQuery('SELECT tweets.id, tweets.content, users.name, users.pictureurl FROM Tweets JOIN users ON users.id = Tweets.userid')
+    .then(function (result) {
+      var tweets = result.rows
       res.render('index', { title: 'All Tweets', tweets: tweets, showForm: true })
     })
   })
 
-  router.get('/users/:name', function (req, res) {
-    client.query('SELECT tweets.content, users.name, users.pictureurl, tweets.id FROM tweets JOIN users on users.id = tweets.userid WHERE name=$1', [req.params.name], function (err, result) {
+  router.get('/users/:name', function(req, res) {
+    clientQuery('SELECT tweets.content, users.name, users.pictureurl, tweets.id FROM tweets JOIN users on users.id = tweets.userid WHERE name=$1', [req.params.name])
+    .then(function (result) {
       var tweets = result.rows;
       var name = tweets[0].name;
       res.render('index', {
@@ -23,36 +34,37 @@ module.exports = function (io, client) {
         showForm: true
       })
     })
-
   })
 
-  router.get('/tweets/:id', function (req, res) {
+  router.get('/tweets/:id', function(req, res) {
     var id = +req.params.id
-        client.query('SELECT tweets.content, users.name, users.pictureurl, tweets.id FROM tweets JOIN users on users.id = tweets.userid WHERE tweets.id=$1', [id], function(err, result){
-          var tweets = result.rows;
-          res.render('index', { title: 'Tweet', tweets: tweets  })
-        })    
-        
+    client.query('SELECT tweets.content, users.name, users.pictureurl, tweets.id FROM tweets JOIN users on users.id = tweets.userid WHERE tweets.id=$1', [id], function(err, result) {
+      var tweets = result.rows;
+      res.render('index', { title: 'Tweet', tweets: tweets })
+    })
   })
 
-  router.post('/tweets', function (req, res) {
-      var name = req.body.name,
-        text = req.body.text
-    client.query('SELECT * FROM users WHERE name=$1', [name], function (err, result) {
+  router.delete('/tweets/:id', function(req, res) {
+    var id = +req.params.id
+  })
+
+  router.post('/tweets', function(req, res) {
+    var name = req.body.name,
+      text = req.body.text
+    client.query('SELECT * FROM users WHERE name=$1', [name], function(err, result) {
       var user = result.rows;
       if (user.length === 0) {
-        client.query('INSERT INTO users (name, pictureurl) VALUES ($1, $2)', [name, 'http://placehold.it/200x200'], function (err, result) {
-        }) 
+        client.query('INSERT INTO users (name, pictureurl) VALUES ($1, $2)', [name, 'http://placehold.it/200x200'], function(err, result) {})
       }
-      client.query('SELECT * FROM users WHERE name=$1', [name], function (err, result) {
+      client.query('SELECT * FROM users WHERE name=$1', [name], function(err, result) {
         var user = result.rows[0];
-        client.query('INSERT INTO tweets (userId, content) VALUES ($1, $2)', [user.id, text], function (err, result) {
+        client.query('INSERT INTO tweets (userId, content) VALUES ($1, $2)', [user.id, text], function(err, result) {
           // io.sockets.emit('new_tweet', )
           res.redirect('/')
         })
       })
     })
-    
+
   })
 
   return router
